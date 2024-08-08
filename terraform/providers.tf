add3c15daae2a444d03c0aebf258ca6efa0f8608
Adam Maas <adamjm1220@gmail.com>
2024-08-08 14:01:26 -0400

    Deploylambda27

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,15 +1,18 @@
-provider "aws" {
-  region     = "us-east-2"
+# provider "aws" {
+#   region     = "us-east-2"
 #   access_key  = var.aws_access_key
 #   secret_key  = var.aws_secret_key
-}
-
-
-# terraform {
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "~> 3.0"  # Or the latest version
-#     }
-#   }
 # }
+
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"  # Or the latest version
+      region     = "us-east-2"
+      access_key  = var.aws_access_key
+      secret_key  = var.aws_secret_key
+    }
+  }
+}
