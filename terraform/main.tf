ef834bbdf5a7a2a5a075e16364342112e9eb6be8
Adam Maas <adamjm1220@gmail.com>
2024-08-08 14:42:03 -0400

    Deploylambda28

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,14 +11,14 @@ resource "aws_s3_bucket" "example" {
 
 #lambda
 # Create the Lambda function
-resource "aws_lambda_function" "adam-lambda-for-springbootapp" {
-  function_name = "adam-lambda-for-springbootapp"
-  role          = "arn:aws:iam::339712758982:role/lambda_role_cicd"
- # handler       = "com.example.demo.HelloWorldHandler::handleRequest"
-  runtime       = "java11"  # or "java8"
-# The below is if we want to upload directly to lambda, however, we wouldnt be storing on the cloud. best practice it to store the executable file via s3 then run from that. We stil need source_code_hash if we want to use lambda with this method instead of havingthe file avaliable via s3
-   filename     = "DockerCICDPipeline/target/DockerCICDPipeline-0.0.1-SNAPSHOT.jar" #Update this with whatever jar executable file you want to run in the lambda function
- }
+# resource "aws_lambda_function" "adam-lambda-for-springbootapp" {
+#   function_name = "adam-lambda-for-springbootapp"
+#   role          = "arn:aws:iam::339712758982:role/lambda_role_cicd"
+#  # handler       = "com.example.demo.HelloWorldHandler::handleRequest"
+#   runtime       = "java11"  # or "java8"
+# # The below is if we want to upload directly to lambda, however, we wouldnt be storing on the cloud. best practice it to store the executable file via s3 then run from that. We stil need source_code_hash if we want to use lambda with this method instead of havingthe file avaliable via s3
+#    filename     = "DockerCICDPipeline/target/DockerCICDPipeline-0.0.1-SNAPSHOT.jar" #Update this with whatever jar executable file you want to run in the lambda function
+#  }
 
 
 #Lambda function for TF
