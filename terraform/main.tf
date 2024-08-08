5a11d0abc4c3f03df57ee26ac98c2bbac324a384
Adam Maas <adamjm1220@gmail.com>
2024-08-08 14:36:12 -0400

    Deploylambda28

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,7 +13,7 @@ resource "aws_s3_bucket" "example" {
 # Create the Lambda function
 resource "aws_lambda_function" "adam-lambda-for-springbootapp" {
   function_name = "adam-lambda-for-springbootapp"
-  role          = aws_iam_role.lambda_role_cicd.arn
+  role          = "arn:aws:iam::339712758982:role/lambda_role_cicd"
   handler       = "com.example.demo.HelloWorldHandler::handleRequest"
   runtime       = "java11"  # or "java8"
 # The below is if we want to upload directly to lambda, however, we wouldnt be storing on the cloud. best practice it to store the executable file via s3 then run from that. We stil need source_code_hash if we want to use lambda with this method instead of havingthe file avaliable via s3
@@ -22,26 +22,9 @@ resource "aws_lambda_function" "adam-lambda-for-springbootapp" {
 
 
 #Lambda function for TF
-# Create an IAM role for the Lambda function
-resource "aws_iam_role" "lambda_role_cicd" {
-  name = "lambda_role_cicd"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        },
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-}
 
 # Attach the basic execution policy to the role
-resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
-  role       = aws_iam_role.lambda_role_cicd.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
+# resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
+#   role       = aws_iam_role.lambda_role_cicd.name
+#   policy_arn = "arn:aws:iam::339712758982:role/lambda_role_cicd"
+# }
