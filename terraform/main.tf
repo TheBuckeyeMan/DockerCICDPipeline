6b20d459905e8111bb95cf7bfcb763435ff5a4bc
Adam Maas <adamjm1220@gmail.com>
2024-08-08 10:23:22 -0400

    tftest16

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
-resource "aws_s3_bucket" "Adams_test_bucket" {
-  bucket = "Adams_test_bucket_for_cicd_unique_name_76492615"  # Replace with a unique bucket name
+resource "aws_s3_bucket" "adams_test_bucket" {
+  bucket = "adams_test_bucket_for_cicd_unique_name_76492615"  # Replace with a unique bucket name
   tags = {
-    Name        = "Adams_test_bucket"
+    Name        = "adams_test_bucket"
     Environment = "dev"
   }
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,4 @@
 output "bucket_name" {
-  value = aws_s3_bucket.Adams_test_bucket.bucket
+  value = aws_s3_bucket.adams_test_bucket.bucket
   description = "The name of the S3 bucket"
 }
