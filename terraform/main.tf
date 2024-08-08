c7d98df7522ad72b63e3d327fce7357e4a7d92c1
Adam Maas <adamjm1220@gmail.com>
2024-08-08 14:24:35 -0400

    Deploylambda28

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,60 +10,38 @@ resource "aws_s3_bucket" "example" {
 
 
 #lambda
-# IAM Role for Lambda Function
+# Create the Lambda function
+resource "aws_lambda_function" "adam-lambda-for-springbootapp" {
+  function_name = "adam-lambda-for-springbootapp"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "com.example.demo.HelloWorldHandler::handleRequest"
+  runtime       = "java11"  # or "java8"
+# The below is if we want to upload directly to lambda, however, we wouldnt be storing on the cloud. best practice it to store the executable file via s3 then run from that. We stil need source_code_hash if we want to use lambda with this method instead of havingthe file avaliable via s3
+   filename     = "/Users/adammaas/Desktop/GitHub_Repositories/DockerCICDPipeline/target/DockerCICDPipeline-0.0.1-SNAPSHOT.jar" #Update this with whatever jar executable file you want to run in the lambda function
+ }
+
+
+#Lambda function for TF
+# Create an IAM role for the Lambda function
 resource "aws_iam_role" "lambda_role" {
-  name = "lambda_execution_role"
+  name = "lambda_role"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
+    Version = "2012-10-17",
     Statement = [
       {
-        Action = "sts:AssumeRole",
         Effect = "Allow",
         Principal = {
           Service = "lambda.amazonaws.com"
-        }
+        },
+        Action = "sts:AssumeRole"
       }
     ]
   })
 }
 
-# IAM Policy for Lambda Execution
-resource "aws_iam_policy" "lambda_policy" {
-  name        = "lambda_execution_policy"
-  description = "Policy for Lambda execution"
-  policy      = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action   = [
-          "logs:*",
-          "cloudwatch:*",
-          "s3:*"  # Add other necessary permissions
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      }
-    ]
-  })
-}
-
-# Attach Policy to Role
+# Attach the basic execution policy to the role
 resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
   role       = aws_iam_role.lambda_role.name
-  policy_arn = aws_iam_policy.lambda_policy.arn
-}
-
-# Lambda Function
-resource "aws_lambda_function" "empty_lambda" {
-  function_name = "empty_lambda_function"
-
-#   # Replace with the path to your Lambda deployment package
-#   s3_bucket        = "your-s3-bucket-name"  # The bucket containing the Lambda code
-#   s3_key           = "path/to/your/lambda.zip"  # The path to your Lambda deployment package in the S3 bucket
-#   handler          = "index.handler"  # The function within your code that Lambda calls to begin execution
-#   runtime          = "nodejs14.x"  # Adjust based on the runtime you are using
-  role             = aws_iam_role.lambda_role.arn
-
-#   # Lambda function code can be empty or you can specify a placeholder file if needed
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
