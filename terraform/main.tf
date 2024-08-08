920e69720400a794ae9295d6c394735e2be23ca2
Adam Maas <adamjm1220@gmail.com>
2024-08-08 13:45:26 -0400

    Deploylambda23

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,22 +1,97 @@
-#s3 bucket
-resource "aws_s3_bucket" "adams-test-bucket" {
-  bucket = "adams-test-bucket-for-cicd-unique-name-76492615"  # Replace with a unique bucket name
-  tags = {
-    Name        = "adams-test-bucket"
-    Environment = "dev"
-  }
+# #s3 bucket
+# resource "aws_s3_bucket" "adams-test-bucket" {
+#   bucket = "adams-test-bucket-for-cicd-unique-name-76492615"  # Replace with a unique bucket name
+#   tags = {
+#     Name        = "adams-test-bucket"
+#     Environment = "dev"
+#   }
+# }
+# #Enables s3 bucket versioning so we can overrite the s3 bucket and not fail terraform apply 
+# resource "aws_s3_bucket_acl" "adams-test-bucket" {
+#   bucket = aws_s3_bucket.adams-test-bucket.id
+#   acl    = "private"
+# }
+# resource "aws_s3_bucket_versioning" "adams-test-bucket" {
+#   bucket = aws_s3_bucket.adams-test-bucket.id
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+# }
+
+resource "aws_s3_bucket" "football-data-s3-adam-maas-12202" {
+  bucket = "football-data-s3-adam-maas-12202"  # Change to your desired bucket name
 }
-#Enables s3 bucket versioning so we can overrite the s3 bucket and not fail terraform apply 
-resource "aws_s3_bucket_acl" "adams-test-bucket" {
-  bucket = aws_s3_bucket.adams-test-bucket.id
-  acl    = "private"
+
+resource "aws_s3_bucket_public_access_block" "football-data-s3-adam-maas-12202_public_access" {
+  bucket = aws_s3_bucket.football-data-s3-adam-maas-12202.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
-resource "aws_s3_bucket_versioning" "adams-test-bucket" {
-  bucket = aws_s3_bucket.adams-test-bucket.id
-  versioning_configuration {
-    status = "Enabled"
-  }
+
+//Add in the java executable jar file to the s3 bucket
+# resource "aws_s3_object" "lambda_jar" {
+#   bucket = aws_s3_bucket.football-data-s3-adam-maas-12202.bucket
+#   key    = "APIData-0.0.1-SNAPSHOT.jar"
+#   source = "/Users/adammaas/Desktop/GitHub_Repositories/s3_data_landing/save_file_to_s3_springboot_app/target/APIData-0.0.1-SNAPSHOT.jar"
+# }
+
+
+
+//IAM Roles associated with the sd3 bucket resource "aws_iam_policy" "s3_bucket_creation_policy" {
+resource "aws_iam_policy" "s3_bucket_creation_policy" {
+  name        = "S3BucketCreationPolicy"
+  description = "Policy to allow creating S3 buckets"
+  policy      = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = [
+          "s3:CreateBucket",
+          "s3:PutBucketAcl",
+          "s3:PutBucketPolicy",
+          "s3:PutBucketLogging",
+          "s3:PutBucketNotification",
+          "s3:PutBucketTagging",
+          "s3:PutBucketVersioning",
+          "s3:PutBucketWebsite"
+        ]
+        Resource = "arn:aws:s3:::*"
+      }
+    ]
+  })
 }
+
+resource "aws_iam_user_policy_attachment" "example_user_attach_policy" {
+  user       = aws_iam_user.example_user.name
+  policy_arn = aws_iam_policy.s3_bucket_creation_policy.arn
+}
+
+resource "aws_iam_user_policy_attachment" "example_user_attach_s3_full_access" {
+  user       = aws_iam_user.example_user.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # #Lambda reosurce
 # resource "aws_lambda_function" "my_lambda_function" {
