295e8669e2ff18d93053a3d8fc1560ed3f226539
Adam Maas <adamjm1220@gmail.com>
2024-08-08 10:56:16 -0400

    Deploylambda1

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,39 +9,39 @@ resource "aws_s3_bucket" "adams-test-bucket" {
 
 
 
-# #Lambda reosurce
-# resource "aws_lambda_function" "my_lambda_function" {
-#   function_name = "my_lambda_function"
-#   role          = aws_iam_role.lambda_exec.arn
-#   handler       = "index.handler"
-#   runtime       = "java11"  # Update to the runtime you're using, e.g., java11, nodejs14.x, etc.
+#Lambda reosurce
+resource "aws_lambda_function" "my_lambda_function" {
+  function_name = "my_lambda_function"
+  role          = aws_iam_role.lambda_exec.arn
+  handler       = "index.handler"
+  runtime       = "java11"  # Update to the runtime you're using, e.g., java11, nodejs14.x, etc.
 
-#   # Ensure you have a ZIP file of your Lambda function code
-#    s3_bucket = aws_s3_bucket.adams-test-bucket.bucket
-# #   s3_key    = "path/to/your/lambda-deployment-package.zip"
-# }
+  # Ensure you have a ZIP file of your Lambda function code
+   s3_bucket = aws_s3_bucket.adams-test-bucket.bucket
+   s3_key    = "DockerCICDPipeline-0.0.1-SNAPSHOT.jar"
+}
 
-# #Lambda IAM role:
-# resource "aws_iam_role" "lambda_exec" {
-#   name = "lambda_exec_role"
+#Lambda IAM role:
+resource "aws_iam_role" "lambda_exec" {
+  name = "lambda_exec_role"
 
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Action = "sts:AssumeRole"
-#         Effect = "Allow"
-#         Principal = {
-#           Service = "lambda.amazonaws.com"
-#         }
-#       },
-#     ]
-#   })
-# }
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
 
-# resource "aws_iam_role_policy_attachment" "lambda_policy" {
-#   role       = aws_iam_role.lambda_exec.name
-#   policy_arn  = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-# }
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn  = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
 
 
