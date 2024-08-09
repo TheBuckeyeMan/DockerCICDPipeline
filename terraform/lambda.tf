86b5e40c16f1d8ab46676b69557568869bc31f8f
Adam Maas <adamjm1220@gmail.com>
2024-08-09 10:36:00 -0400

    FinalCICD

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -31,15 +31,15 @@ data "aws_caller_identity" "current" {}
 
 #The Resource - AWS LAMBDA - Below Works as intended, uploads jar file specified
 
-resource "aws_lambda_function" "adam_lambda_function" {
-  function_name = "adam-lambda-for-cicd"
-  role          = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/lambda_role_cicd"
-  handler       = "com.example.demo.HelloWorldHandler::handleRequest"
-  runtime       = "java11"  # or any runtime you prefer
-  filename         = "../target/DockerCICDPipeline-0.0.1-SNAPSHOT.jar" # or provide a path to an actual deployment package
-  source_code_hash = filebase64sha256("../target/DockerCICDPipeline-0.0.1-SNAPSHOT.jar")
-  timeout     = 5
-}
+# resource "aws_lambda_function" "adam_lambda_function" {
+#   function_name = "adam-lambda-for-cicd"
+#   role          = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/lambda_role_cicd"
+#   handler       = "com.example.demo.HelloWorldHandler::handleRequest"
+#   runtime       = "java11"  # or any runtime you prefer
+#   filename         = "../target/DockerCICDPipeline-0.0.1-SNAPSHOT.jar" # or provide a path to an actual deployment package
+#   source_code_hash = filebase64sha256("../target/DockerCICDPipeline-0.0.1-SNAPSHOT.jar")
+#   timeout     = 5
+# }
 
-# Data source to fetch the current AWS account ID
-data "aws_caller_identity" "current" {}
+# # Data source to fetch the current AWS account ID
+# data "aws_caller_identity" "current" {}
