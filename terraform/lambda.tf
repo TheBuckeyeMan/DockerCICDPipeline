0cc35fbdc0d7f5c554755f6a4fb6911f655570a1
Adam Maas <adamjm1220@gmail.com>
2024-08-09 10:35:02 -0400

    Revert

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,27 +1,43 @@
+#Deploy from Docker Image
+resource "aws_lambda_function" "adam_lambda_function" {
+  function_name = "adam-lambda-for-cicd"
+  role          = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/lambda_role_cicd"
+  package_type   = "Image"  # Specify that you are deploying from a container image
+  image_uri      = "thebuckeyeman20/cicd:image1"  # Replace with your Docker image <username(not email)/repo name:tag>
+  timeout     = 5
+}
+
+# Data source to fetch the current AWS account ID
+data "aws_caller_identity" "current" {}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#The Resource - AWS LAMBDA - Below Works as intended, uploads jar file specified
+
 resource "aws_lambda_function" "adam_lambda_function" {
   function_name = "adam-lambda-for-cicd"
   role          = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/lambda_role_cicd"
   handler       = "com.example.demo.HelloWorldHandler::handleRequest"
   runtime       = "java11"  # or any runtime you prefer
-
-  # If you don't have a deployment package, you can leave these fields commented out or empty
-  # filename     = "./target/DockerCICDPipeline-0.0.1-SNAPSHOT.jar"
-  # source_code_hash = filebase64sha256("./target/DockerCICDPipeline-0.0.1-SNAPSHOT.jar")
-
-  # You can provide an empty deployment package if required
   filename         = "../target/DockerCICDPipeline-0.0.1-SNAPSHOT.jar" # or provide a path to an actual deployment package
   source_code_hash = filebase64sha256("../target/DockerCICDPipeline-0.0.1-SNAPSHOT.jar")
-
-  # Optionally, you can set up environment variables
-#   environment {
-#     variables = {
-#       ENV_VAR_1 = "value1"
-#       ENV_VAR_2 = "value2"
-#     }
-#   }
-
-  # You can also set up other options like memory size, timeout, etc.
-  #memory_size = 128
   timeout     = 5
 }
 
